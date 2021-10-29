e17ddb84413bf2657407f9b17b465026dbf538a5
James <jcox@findmypast.com>
2021-10-29 09:59:03 +0100

    refactored logic which finds inline and reference links

--- bad-links-in-markdown.js
+++ bad-links-in-markdown.js
@@ -26,25 +26,26 @@ export const badLinksInMarkdown = async (topLevelDirectory) => {
 const MARKDOWN_INLINE_LINK_REGEX = /\[.*?\]\(.*?\)/g;
 const findInlineMarkdownLinks = (markdown) => {
   const allInlineLinks = markdown.match(MARKDOWN_INLINE_LINK_REGEX) || [];
-  return allInlineLinks.map((inlineLink) => {
-    const linkWithTag = inlineLink.match(/[(](.*)[)]/)[1];
-    const [link, tag] = linkWithTag.startsWith("#")
-      ? [linkWithTag, undefined]
-      : linkWithTag.split("#");
-    return { markdownLink: inlineLink, link, tag };
-  });
+  return allInlineLinks.map((inlineLink) => ({
+    markdownLink: inlineLink,
+    ...makeLinkObject(inlineLink.match(/[(](.*)[)]/)[1]),
+  }));
 };
 
 const MARKDOWN_REFERENCE_LINK_REGEX = /\[.*?\]:.*/g;
 const findReferenceMarkdownLinks = (markdown) => {
   const allReferenceLinks = markdown.match(MARKDOWN_REFERENCE_LINK_REGEX) || [];
-  return allReferenceLinks.map((referenceLink) => {
-    const linkWithTag = referenceLink.match(/\[.*?\]:(.*)$/)[1];
-    const [link, tag] = linkWithTag.startsWith("#")
-      ? [linkWithTag, undefined]
-      : linkWithTag.split("#");
-    return { markdownLink: referenceLink, link, tag };
-  });
+  return allReferenceLinks.map((referenceLink) => ({
+    markdownLink: referenceLink,
+    ...makeLinkObject(referenceLink.match(/\[.*?\]:(.*)$/)[1]),
+  }));
+};
+
+const makeLinkObject = (linkWithTag) => {
+  const [link, tag] = linkWithTag.startsWith("#")
+    ? [linkWithTag, undefined]
+    : linkWithTag.split("#");
+  return { link, tag };
 };
 
 if (module === require.main) {
